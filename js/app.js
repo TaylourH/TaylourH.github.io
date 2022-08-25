a41726c25ceae7d2986f806da662e188ee27911c
TaylourH <harristaylour@gmail.com>
2022-08-25 09:00:17 -0400

    under construction notice

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+
+
+function underConstruction() {
+// document.querySelector("body").onload.alert("This site is currently under construction! Sorry for the inconvenience. Please visit again soon.");
+alert("This site is currently under construction! Sorry for the inconvenience. Please visit again soon.");
+
+}
